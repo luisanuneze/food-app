47b74a9adc99db993115a13598ead32a3fa8d3cb
Luisa <luisa.nunez150@gmail.com>
2021-05-20 13:35:23 -0600

    Adding cart with dynamic information

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,44 +1,56 @@
 <template>
   <main class="cart container">
-    <h2>Your Cart</h2>
+    <head> </head>
+    <body>
+      <h2>Your Cart</h2>
 
-    <section v-if="cart.length">
-      <table>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Add Ons</th>
-            <th>Amount</th>
-            <th>Total Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in cart" :key="item.id">
-            <td>
-              {{ item.item }}
-              <span v-if="item.options">- {{ item.options }}</span>
-            </td>
-            <td>
-              <span v-for="addon in item.addOns" :key="addon" class="comma">
-                {{ addon }}
-              </span>
-            </td>
-            <td>{{ item.count }}</td>
-            <td>{{ item.combinedPrice }}</td>
-          </tr>
-          <tr>
-            <td colspan="3"></td>
-            <td class="total">Total: ${{ totalPrice.toFixed(2) }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
+      <section v-if="cart.length">
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Add Ons</th>
+              <th>Amount</th>
+              <th>Total Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in cart" :key="item.id">
+              <td>
+                {{ item.item }}
+                <span v-if="item.options"> - {{ item.options }}</span>
+              </td>
+              <td>
+                <span v-for="addon in item.addOns" :key="addon" class="comma">
+                  {{ addon }}
+                </span>
+              </td>
+              <td>{{ item.count }}</td>
+              <td>{{ item.combinedPrice }}</td>
+            </tr>
+            <tr>
+              <td colspan="3"></td>
+              <td class="total">Total: ${{ totalPrice.toFixed(2) }}</td>
+            </tr>
+          </tbody>
+        </table>
 
-    <AppEmptyCart v-else />
+        <button id="checkout-button" class="primary">Checkout</button>
+      </section>
+
+      <AppEmptyCart v-else />
+
+      <script src="https://js.stripe.com/v3/"></script>
+      <script charset="utf-8">
+        var stripe = Stripe(
+          'pk_test_51ItEfnBjTD3GmzG9oqSFqdwPHFxKe4tbHbQDC2liBgUrzT5HbqFUCODllaThkwhSGpbdzQTfa3uFMI1ib3MCSykg00guhO8DNi'
+        )
+      </script>
+    </body>
   </main>
 </template>
 
-<script>
+<scrip>
 import AppEmptyCart from '@/components/AppEmptyCart.vue'
 
 export default {
@@ -54,6 +66,6 @@ export default {
     },
   },
 }
-</script>
+</scrip>
 
 <style lang="scss" scoped></style>
