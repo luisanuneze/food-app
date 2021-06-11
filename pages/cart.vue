2fc2026a283df4d8ca81943a35dda220300de01e
Luisa <luisa.nunez150@gmail.com>
2021-06-11 09:16:14 -0600

    Update

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,45 +1,40 @@
 <template>
   <main class="cart container">
-    <head> </head>
-    <body>
-      <h2>Your Cart</h2>
+    <h2>Your Cart</h2>
 
-      <section v-if="cart.length">
-        <table>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Add Ons</th>
-              <th>Amount</th>
-              <th>Total Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in cart" :key="item.id">
-              <td>
-                {{ item.item }}
-                <span v-if="item.options"> - {{ item.options }}</span>
-              </td>
-              <td>
-                <span v-for="addon in item.addOns" :key="addon" class="comma">
-                  {{ addon }}
-                </span>
-              </td>
-              <td>{{ item.count }}</td>
-              <td>{{ item.combinedPrice }}</td>
-            </tr>
-            <tr>
-              <td colspan="3"></td>
-              <td class="total">Total: ${{ totalPrice.toFixed(2) }}</td>
-            </tr>
-          </tbody>
-        </table>
+    <section v-if="cart.length">
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Add Ons</th>
+            <th>Amount</th>
+            <th>Total Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in cart" :key="item.id">
+            <td>
+              {{ item.item }}
+              <span v-if="item.options">- {{ item.options }}</span>
+            </td>
+            <td>
+              <span v-for="addon in item.addOns" :key="addon" class="comma">{{
+                addon
+              }}</span>
+            </td>
+            <td>{{ item.count }}</td>
+            <td>{{ item.combinedPrice }}</td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td class="total">Total: ${{ totalPrice.toFixed(2) }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
 
-        <button id="checkout-button" class="primary">Checkout</button>
-      </section>
-
-      <AppEmptyCart v-else />
-    </body>
+    <AppEmptyCart v-else />
   </main>
 </template>
 
